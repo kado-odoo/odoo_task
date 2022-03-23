70aeb136110a58a52106648b0ed684cfa8baa781
“kado-odoo” <kado@odoo.com>
2022-03-23 12:12:45 +0530

    [MODIFY] Module Name And Manifest File Change.

---
+++ name_get_custom/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    #  Information
+    'name':'Name Get',
+    'version': '15.0',
+    'summary': 'Name Get',
+    'description':'Name Get',
+    'category':'',
+
+    # Author
+    'author': 'Odoo Ps',
+    'website': 'https://www.odoo.com/',
+    'license': '',
+    
+    # Dependency
+    'data':[],
+    'depends':['sale_management'],
+    'demo':[],
+    # Other
+    'application':True,
+    'installble':True,
+}
